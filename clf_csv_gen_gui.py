a11fff4d432052d916317b06b33254f8ea2a2ddd
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-04-15 21:58:37 -0300

    ordenando

--- clf_csv_gen_gui.py
+++ clf_csv_gen_gui.py
@@ -18,10 +18,12 @@ from PyQt5.QtWidgets import (
     QFileDialog,
     QMessageBox,
     QGridLayout,
+    QHBoxLayout,
     QPushButton,
     QLabel,
     QLineEdit,
     QWidget,
+    QRadioButton,
 )
 
 
@@ -34,8 +36,10 @@ window.setGeometry(100, 100, 800, 240)
 
 layout = QGridLayout()
 
+## Row pretty label 
 layout.addWidget(QLabel("<b>Input:</b>"),0,0)
 
+## Row images directory
 base_dir_btn=QPushButton("Base directory:");
 base_dir_linedit=QLineEdit('./');
 layout.addWidget(base_dir_btn,1,0)
@@ -44,12 +48,15 @@ def on_base_dir_btn_clicked():
     filepath = str(QFileDialog.getExistingDirectory(window, "Select Directory"))
     base_dir_linedit.setText(filepath)
 
+## Row images filetype
 file_type_linedit=QLineEdit('.png')
 layout.addWidget(QLabel("file type:"),2,0)
 layout.addWidget(file_type_linedit,2,1)
 
+## Row pretty label 
 layout.addWidget(QLabel("<b>Output:</b>"),3,0)
 
+## Row CSV directory
 csv_dir_btn=QPushButton("CSV file directory:")
 csv_dir_linedit=QLineEdit('./')
 layout.addWidget(csv_dir_btn,4,0)
@@ -58,15 +65,32 @@ def on_csv_dir_btn_clicked():
     filepath = str(QFileDialog.getExistingDirectory(window, "Select Directory"))
     csv_dir_linedit.setText(filepath)
 
+## Row CSV filename
 csv_filename_linedit=QLineEdit('labels.csv')
 layout.addWidget(QLabel("CSV filename:"),5,0)
 layout.addWidget(csv_filename_linedit,5,1)
 
+## Row category
+hlayout = QHBoxLayout();
+rb_last = QRadioButton('Last folder name', window);
+rb_last.setChecked(False);
+rb_first = QRadioButton('First folder name', window);
+rb_first.setChecked(True);
+hlayout.addWidget(rb_last);
+hlayout.addWidget(rb_first);
+layout.addWidget(QLabel("Category:"),6,0)
+layout.addLayout(hlayout,6,1);
+
+## Row generate_btn
 def on_generate_btn_clicked():
     base_dir     = base_dir_linedit.text();
     file_type    = file_type_linedit.text();
     csv_dir      = csv_dir_linedit.text();
     csv_filename = csv_filename_linedit.text();
+    
+    rb_first_down=rb_first.isChecked();
+    
+    print('rb_first_down',rb_first_down)
     
     format_list=[file_type];
     csv_file=os.path.join(csv_dir,csv_filename);
@@ -84,10 +108,11 @@ def on_generate_btn_clicked():
     alert.exec()
 
 generate_btn=QPushButton("Generate")
-layout.addWidget(QLabel(" "),6,0)
-layout.addWidget(generate_btn,7,0)
+layout.addWidget(QLabel(" "),7,0)
+layout.addWidget(generate_btn,8,0)
 window.setLayout(layout)
 
+##
 csv_dir_btn.clicked.connect(on_csv_dir_btn_clicked)
 base_dir_btn.clicked.connect(on_base_dir_btn_clicked)
 generate_btn.clicked.connect(on_generate_btn_clicked)
